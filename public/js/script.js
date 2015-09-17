ebe4a3b6f14df5a444e779f9bfd1f361ff889b33
EmanuelDev <emaborbely19@gmail.com>
2015-09-17 22:30:12 +0300

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('.login_link').click(function() {
+        $('.toggle').slideToggle();
+    });
+     
+    $(".extra_information").click(function() {
+        $(this).next('.description_toggle').slideToggle();
+        $(this).next('number_tab').css("background-color","black");
+    });
+     
+    $('#viewMore').click(function() {
+        $('#hiddenRow').slideToggle();
+    });
+    
+    $(".more_information").click(function() {
+        $(this).next('.second_description_toggle').fadeToggle();
+    });
+    
+    
+});
+
