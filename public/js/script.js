50b7806db6edb44f6d113607dc65a3ab949b65b0
EmanuelDev <emaborbely19@gmail.com>
2015-09-18 03:18:19 +0300

    Last version before testing

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,21 @@ $(document).ready(function() {
      
     $(".extra_information").click(function() {
         $(this).next('.description_toggle').slideToggle();
-        $(this).next('number_tab').css("background-color","black");
+        var $this = $(this);
+		$this.toggleClass('.extra_information');
+		if($this.hasClass('.extra_information')){
+			$this.text('-');			
+		}else{
+			$this.text('+').css({
+                'position': 'relative',
+                'float': 'right',
+                'bottom': '72px',
+                'right': '10px',
+                'font-family': 'Raleway',
+                'font-weight': '600',
+                'font-size': '36px'
+            });
+		}
     });
      
     $('#viewMore').click(function() {
@@ -14,6 +28,28 @@ $(document).ready(function() {
     
     $(".more_information").click(function() {
         $(this).next('.second_description_toggle').fadeToggle();
-    });      
+        var $this = $(this);
+		$this.toggleClass('more_information');
+		if($this.hasClass('more_information')){
+			$this.text('+');			
+		} else {
+			$this.text('-').css({
+                'position': 'relative',
+                'bottom': '-2px',
+                'color': '#fff', 
+                'background': '#53B8C4',
+                'width': '40px',
+                'height': '40px',
+                'padding': '3px',
+                'float': 'left',
+                'font-size': '28px',
+                'text-align': 'center',
+                'cursor': 'pointer',
+                'font-family': 'Raleway',
+                'font-weight': 'bold'
+            });
+		}
+    });  
+    
 });
 
